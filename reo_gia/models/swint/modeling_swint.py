7adef882e8c9ea5473c2db23d1e637fc625fa712
Soyeon Choi <132669940+clr4takeoff@users.noreply.github.com>
2026-02-20 19:56:28 -0800

    Fix: Update CIFAR-10 model reference in Swin

--- reo_gia/models/swint/modeling_swint.py
+++ reo_gia/models/swint/modeling_swint.py
@@ -5,7 +5,7 @@ from ..base import BaseModel, ModelRegistry
 class SwinForImageClassification(BaseModel, _SwinForImageClassification):
     model_id = "microsoft/swin-tiny-patch4-window7-224"
     model_variation = ModelRegistry(**{
-        'default': model_id, 'imagenet-1k': model_id, 'cifar-10': "nielsr/swin-tiny-patch4-window7-224-finetuned-cifar10"
+        'default': model_id, 'imagenet-1k': model_id, 'cifar-10': "ahirtonlopes/swin-tiny-patch4-window7-224-finetuned-cifar10"
     })
 
 
